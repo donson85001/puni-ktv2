f5f6c564671dc197b8d3343983957426506f48ca
donson85001 <donson85001@gmail.com>
2026-03-05 15:10:25 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,11 +1,11 @@
-// common.js
-// 把這裡改成你 Apps Script Web App 的部署網址（/exec）
-const API = "https://script.google.com/macros/s/AKfycbzzE30Yr6i4L2LkrMI_Jrku-7y9ENSrYtL5kLT0POs4NsNQci6IGB6FJFHp5LecNalN/exec";
-
-async function api(action, params={}){
-  const url = new URL(API);
-  url.searchParams.set("action", action);
-  Object.entries(params).forEach(([k,v])=>url.searchParams.set(k, v));
-  const r = await fetch(url.toString(), { method: "GET" });
-  return r.json();
+function respond_(e, obj) {
+  const cb = e.parameter.callback;
+  if (cb) {
+    return ContentService
+      .createTextOutput(`${cb}(${JSON.stringify(obj)})`)
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+  }
+  return ContentService
+    .createTextOutput(JSON.stringify(obj))
+    .setMimeType(ContentService.MimeType.JSON);
 }
